5147572285f9baca38f4375647a585da7588ce6a
jgabaut <109908086+jgabaut@users.noreply.github.com>
2024-05-17 07:31:53 +0200

    fix: missing NULL prompt arg

--- src/s4c_gui.c
+++ src/s4c_gui.c
@@ -140,7 +140,7 @@ TextField new_TextField_centered(size_t max_size, int height, int width, int bou
 
 TextField new_TextField_linted(TextField_Linter** linters, size_t num_linters, const void** linter_args, size_t max_size, int height, int width, int start_x, int start_y)
 {
-    return new_TextField_(&warn_TextField, linters, num_linters, linter_args, max_size, height, width, start_x, start_y, S4C_GUI_MALLOC, S4C_GUI_CALLOC, NULL);
+    return new_TextField_(&warn_TextField, linters, num_linters, linter_args, max_size, height, width, start_x, start_y, NULL, S4C_GUI_MALLOC, S4C_GUI_CALLOC, NULL);
 }
 
 TextField new_TextField_alloc(size_t max_size, int height, int width, int start_x, int start_y, s4c_gui_malloc_func* malloc_func, s4c_gui_calloc_func* calloc_func, s4c_gui_free_func* free_func)
